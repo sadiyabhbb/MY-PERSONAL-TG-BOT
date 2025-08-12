c7aa25737ba9fec259c2dcb9565260f3015518e7
sadiyabhbb <sadiyabhbb@proton.me>
2025-08-12 11:06:54 +0600

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,1 +1,31 @@
+export default {
+  ownerId: process.env.OWNER_ID ? Number(process.env.OWNER_ID) : null,
 
+  whitelist: [
+    123456789,
+    987654321,
+  ],
+
+  blacklist: [
+    111111111,
+    222222222,
+  ],
+
+  prefix: "/",
+
+  roles: {
+    admins: [123456789],
+    mods: [333333333, 444444444],
+    users: [],
+  },
+
+  session: {
+    enabled: true,
+    storeFile: "./data/session.json",
+  },
+
+  options: {
+    debug: false,
+    logMessages: true,
+  },
+};
