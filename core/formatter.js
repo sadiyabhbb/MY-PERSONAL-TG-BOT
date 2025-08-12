042b39fc9396eb44c67d3a22b050665ed973e7e7
sadiyabhbb <sadiyabhbb@proton.me>
2025-08-12 11:07:40 +0600

    Update formatter.js

--- core/formatter.js
+++ core/formatter.js
@@ -1,1 +1,19 @@
+export function escapeMarkdownV2(text) {
+  return text.replace(/[_*[\]()~`>#+\-=|{}.!]/g, "\\$&");
+}
 
+export function escapeMarkdown(text) {
+  return text.replace(/([_*`\[\]])/g, "\\$1");
+}
+
+export function sanitizeHTML(text) {
+  return text.replace(/[<>&"]/g, (c) => {
+    switch (c) {
+      case "<": return "&lt;";
+      case ">": return "&gt;";
+      case "&": return "&amp;";
+      case "\"": return "&quot;";
+      default: return c;
+    }
+  });
+    }
